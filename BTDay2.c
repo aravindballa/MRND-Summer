ecac8d375dd0fb9262c7a1cbb5dc053521cb7b23
Aravind Balla <bsaaravind@gmail.com>
2016-06-13 16:08:54 +0530

    b tree for two tables - storing

--- BTDay2.c
+++ BTDay2.c
@@ -13,19 +13,21 @@ typedef struct TeachingStaff{
 	char name[24];
 	char dept[12];
 	char college[24];
-} teachingstaff;
+} teachingstaff; //64
 
 typedef struct TLeafPage{
 	int pagetype;
 	int tableId;
 	teachingstaff data[7];
-	char unused[56];
-} tleafpage;
+	char unused[52];
+	int index;
+} tleafpage; //512
 
 typedef struct LeafPage{
 	int pagetype;
+	int tableId;
 	student data[10];
-	char unused[24];
+	char unused[20];
 	int index;
 } leafpage; //512
 
@@ -45,6 +47,11 @@ void PrintStudent(student stud)
 	printf("%d %s %s\n", stud.rollno, stud.name, stud.college);
 }
 
+void PrintTeacher(teachingstaff stud)
+{
+	printf("%d %s %s %s\n", stud.staffid, stud.name, stud.college, stud.dept);
+}
+
 void BuildBtree(char* csv, char* bin)
 {
 	FILE* fp = fopen(csv, "rb+");
@@ -54,7 +61,6 @@ void BuildBtree(char* csv, char* bin)
 	nonleafpage nlpage;
 	memset(&nlpage, 0, sizeof(nonleafpage));
 	fwrite(&nlpage, sizeof(nonleafpage), 1, fw);
-	//fclose(fw);
 
 	nlpage.pagetype = 2;
 	j = 0;
@@ -62,8 +68,6 @@ void BuildBtree(char* csv, char* bin)
 	{
 		leafpage lpage;
 		memset(&lpage, 0, sizeof(leafpage));
-
-		//fw = fopen(bin, "wb+");
 
 		lpage.pagetype = 1;
 
@@ -116,9 +120,6 @@ void BuildBtree2(char* csv, char* bin)
 
 	while (!feof(fp))
 	{
-		
-		//fwrite(&nlpage, sizeof(nonleafpage), 1, fw);
-		//fclose(fw);
 
 		j = 0;
 		while (j<=recordCount)
@@ -126,8 +127,6 @@ void BuildBtree2(char* csv, char* bin)
 			lpo = 0;
 			leafpage lpage;
 			memset(&lpage, 0, sizeof(leafpage));
-
-			//fw = fopen(bin, "wb+");
 
 			lpage.pagetype = 1;
 
@@ -171,11 +170,117 @@ void BuildBtree2(char* csv, char* bin)
 	fclose(fw);
 }
 
+void BuildBTree3(char* csv, char* tcsv, char* bin)
+{
+	FILE* fp = fopen(csv, "rb+");
+	FILE* fpt = fopen(tcsv, "rb+");
+	FILE* fw = fopen(bin, "wb+");
+	FILE* ft;
+	int i, j, lpo = 0;
+	int recordCount = 0, trecordCount = 0;
+
+	fseek(fw, sizeof(tablesector), SEEK_SET); //first iteration: leaving 64 bytes
+
+	tablesector tsec;
+	memset(&tsec, 0, sizeof(tsec));
+
+	tsec.rootPageIds[0] = 0; //offset of current non leaf page
+	tsec.tableIds[0] = 1; //students
+	tsec.tableIds[1] = 2; //teachers
+
+	nonleafpage nlpage, tnlpage;
+	memset(&nlpage, 0, sizeof(nonleafpage));
+	nlpage.pagetype = 2;
+
+	memset(&tnlpage, 0, sizeof(nonleafpage));
+	tnlpage.pagetype = 2;
+
+	while (!feof(fp) && !feof(fpt))
+	{
+		j = 0;
+		while (j <= recordCount && j <= trecordCount)
+		{
+			lpo = 0;
+			leafpage lpage;
+			memset(&lpage, 0, sizeof(leafpage));
+			lpage.tableId = 1;
+			lpage.pagetype = 1;
+
+			tleafpage tlpage;
+			memset(&tlpage, 0, sizeof(tleafpage));
+			tlpage.tableId = 2;
+			tlpage.pagetype = 1;
+
+			for (i = 0; i < 10; i++)
+			{
+				fscanf(fp, "%d,%[^,],%[^\n]\n", &lpage.data[i].rollno, &lpage.data[i].name, &lpage.data[i].college);
+				PrintStudent(lpage.data[i]);
+				lpage.index = i;
+				recordCount++;
+				if (feof(fp))
+					break;
+			}
+
+			nlpage.offsets[lpo] = ftell(fw);
+			printf("%d ", ftell(fw));
+			fwrite(&lpage, sizeof(leafpage), 1, fw);
+			lpo++;
+
+			if (j > 0){
+				nlpage.keys[j - 1] = lpage.data[0].rollno;
+				printf("%d ", lpage.data[0].rollno);
+			}
+			printf("\n");
+
+			tsec.rootPageIds[0] = ftell(fw);
+			fwrite(&nlpage, sizeof(nonleafpage), 1, fw);
+
+			//teachers
+			for (i = 0; i < 7; i++)
+			{
+				fscanf(fpt, "%d,%[^,],%[^,],%[^\n]\n", &tlpage.data[i].staffid, &tlpage.data[i].name, &tlpage.data[i].college, &tlpage.data[1].dept);
+				PrintTeacher(tlpage.data[i]);
+				tlpage.index = i;
+				trecordCount++;
+				if (feof(fpt))
+					break;
+			}
+
+			tnlpage.offsets[lpo] = ftell(fw);
+			printf("%d ", ftell(fw));
+			fwrite(&tlpage, sizeof(tleafpage), 1, fw);
+			lpo++;
+
+			if (j > 0){
+				tnlpage.keys[j - 1] = tlpage.data[0].staffid;
+				printf("%d ", tlpage.data[0].staffid);
+			}
+			printf("\n");
+
+			tsec.rootPageIds[1] = ftell(fw);
+			fwrite(&tnlpage, sizeof(nonleafpage), 1, fw);
+
+			//update the table sector
+			ft = fopen(bin, "rb+");
+			fwrite(&tsec, sizeof(tablesector), 1, ft);
+			fclose(ft);
+
+			j++;
+
+			if (feof(fp) || feof(fpt)) break;
+		}
+
+	}
+
+	fclose(fp);
+	fclose(fw);
+}
+
 main()
 {
-	//printf("%d %d %d %d", sizeof(student), sizeof(leafpage), sizeof(nonleafpage), sizeof(tablesector));
+	//printf("%d %d %d %d", sizeof(teachingstaff), sizeof(tleafpage), sizeof(nonleafpage), sizeof(tablesector));
 
-	BuildBtree2("640studs.csv", "store.bin");
+	BuildBTree3("640studs.csv", "teacherdata.csv", "store.bin");
 
 	_getch();
 }
